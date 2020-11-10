11416f86bbddffc4d1791864e6a94a79aacbddf8
Martinique <me@martidolce.com>
2020-11-10 12:51:19 -0500

    Made some significant changes to this directory structure.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,7 @@
+# frozen_string_literal: true
+
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def greeting
+  phrase = 'Hello World!'
+end
---
+++ lib/greeting.rb
@@ -0,0 +1,15 @@
+=begin
+Author: Martinique Dolce
+Course: Flatiron School 2020, November 9 - 20201, April 2021
+Contact: me@martidolce.com | https://modis.martidolce.com
+=end
+
+def greeting
+  puts "Hello World"
+end
+greeting
+greeting
+greeting
+greeting
+greeting
+greeting
